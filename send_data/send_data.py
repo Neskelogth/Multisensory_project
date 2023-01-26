5f8fcc2afc3aceca3ecb10767dbdd066d2eb24ab
Samuel Kostadinov <SamuelKostadinovGit@gmail.com>
2023-01-26 16:20:40 +0100

    Changed float to int

--- send_data/send_data.py
+++ send_data/send_data.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    host, port = '192.168.0.10', 65000
+    host, port = '192.168.0.100', 65000
     server_address = (host, port)
 
-    name = 'COM3'
+    name = '/dev/ttyACM0'
     counter = 0
 
     ser = serial.Serial(name, 115200, timeout=1)
@@ -19,18 +19,18 @@ if __name__ == '__main__':
         if ser.in_waiting > 0:
             # The first few lines could be incomplete
             if counter > 3:
-                line = ser.readline().decode('utf-8').rstrip()
+                line = ser.readline().rstrip()
                 line = line.split()
 
-                gyro_x = float(line[3])
-                gyro_y = float(line[6])
-                gyro_z = float(line[9])
+                gyro_x = int(line[3])
+                gyro_y = int(line[6])
+                gyro_z = int(line[9])
 
-                accel_x = float(line[13])
-                accel_y = float(line[16])
-                accel_z = float(line[19])
+                accel_x = int(line[13])
+                accel_y = int(line[16])
+                accel_z = int(line[19])
 
-                message = pack('3f', gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z)
+                message = pack('6i', gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z)
                 sock.sendto(message, server_address)
 
             counter += 1
