7ed351a06d66835765cf587c99e288b014e692a2
Samuel Kostadinov <SamuelKostadinovGit@gmail.com>
2023-01-27 13:14:05 +0100

    Modified send_data to send ints

--- send_data/send_data.py
+++ send_data/send_data.py
@@ -1,12 +1,13 @@
 import serial
 import socket
 from struct import pack
+from math import floor
 
 if __name__ == '__main__':
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    host, port = '192.168.0.100', 65000
+    host, port = '192.168.0.10', 65000
     server_address = (host, port)
 
     name = '/dev/ttyACM0'
@@ -17,20 +18,22 @@ if __name__ == '__main__':
 
     while True:
         if ser.in_waiting > 0:
-            # The first few lines could be incomplete
-            if counter > 3:
-                line = ser.readline().rstrip()
-                line = line.split()
 
-                gyro_x = int(line[3])
-                gyro_y = int(line[6])
-                gyro_z = int(line[9])
+            
+            line = ser.readline().rstrip()
+            line = line.split()
 
-                accel_x = int(line[13])
-                accel_y = int(line[16])
-                accel_z = int(line[19])
+            gyro_x = int(floor(float(line[3])))
+            gyro_y = int(floor(float(line[6])))
+            gyro_z = int(floor(float(line[9])))
 
-                message = pack('6i', gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z)
-                sock.sendto(message, server_address)
+            accel_x = int(floor(float(line[13])))
+            accel_y = int(floor(float(line[16])))
+            accel_z = int(floor(float(line[19])))
+
+	    measures = int(line[21])
+
+            message = pack('7i', gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, measures)
+            sock.sendto(message, server_address)
 
             counter += 1
