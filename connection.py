85c9bb28b1325d01efbb31dd32bbc5fe5ea18ed1
Marco Rosson <marfiu99@gmail.com>
2023-01-28 12:47:36 +0100

    Remote control

---
+++ connection.py
@@ -0,0 +1,46 @@
+# pip install python-osc
+from pythonosc import udp_client
+import argparse
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Cursor
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1",
+      help="The ip of the OSC server")
+  parser.add_argument("--port", type=int, default=5005,
+      help="The port the OSC server is listening on")
+  args = parser.parse_args()
+
+  client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+  client.send_message("/on", 1)
+
+  fig, ax = plt.subplots()
+  ax.set_xlim([-1, 1])
+  ax.set_ylim([-1, 1])
+  ax.set_aspect('equal')
+  ax.grid(True)
+
+  circle = plt.Circle((0, 0), radius=0.1, color='r')
+  ax.add_patch(circle)
+
+  cursor = Cursor(ax, useblit=True, color='red', linewidth=1)
+
+  x = 0
+  y = 0
+
+  def on_move(event):
+    global x,y
+    if event.inaxes == ax:
+        x, y = event.xdata, event.ydata
+        circle.center = x, y
+    fig.canvas.draw_idle()
+    client.send_message("/x", x)
+    client.send_message("/y", -y)
+
+
+  fig.canvas.mpl_connect('motion_notify_event', on_move)
+  plt.show()
+
+  client.send_message("/on", 0)
