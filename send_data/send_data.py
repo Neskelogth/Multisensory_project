8b5f1865e0a0960b26d51cf1224c67a90bfad2f3
Samuel Kostadinov <SamuelKostadinovGit@gmail.com>
2023-02-01 16:13:58 +0100

    Modified for accelerometer and removed some unused stuff

--- send_data/send_data.py
+++ send_data/send_data.py
@@ -1,7 +1,6 @@
 import serial
 import socket
 from struct import pack
-from math import floor
 
 if __name__ == '__main__':
 
@@ -21,13 +20,9 @@ if __name__ == '__main__':
             line = ser.readline().rstrip()
             line = line.split()
 
-            gyro_x = int(float(line[3]) * 1000)
-            gyro_y = int(float(line[6]) * 1000)
-            gyro_z = int(float(line[9]) * 1000)
+            gyro_x = int(float(line[3]))
+            gyro_y = int(float(line[6]))
+            gyro_z = int(float(line[9]))
 
-            accel_x = int(float(line[13]) * 1000)
-            accel_y = int(float(line[16]) * 1000)
-            accel_z = int(float(line[19]) * 1000)
-
-            message = pack('6i', gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z)
+            message = pack('3i', gyro_x, gyro_y, gyro_z)
             sock.sendto(message, server_address)
