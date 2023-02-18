dc36616142a294450489318a24c54b116167e4dd
ElenaBasei <108883130+ElenaBasei@users.noreply.github.com>
2023-02-18 09:35:38 +0100

    Add serial print for communication with raspberry

--- force_plate.ino
+++ force_plate.ino
@@ -12,23 +12,14 @@
 
 int init_time = 5000;
 
-float init_fsrA = 0;
-float init_fsrB = 0;
-float init_fsrC = 0;
-float init_fsrD = 0;
-float init_fsrE = 0;
-float init_fsrF = 0;
-float init_fsrG = 0;
-float init_fsrH = 0;
-
-float taraA=0.5656;
-float taraC=0.6219;
-float taraE=0.5605;
-float taraG=0.5556;
-
 const float VCC = 5;
 const float R_DIV  = 10000.0;
 const int avg_size = 10;
+
+float taraA;
+float taraC;
+float taraE;
+float taraG;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -88,19 +79,11 @@ void loop() {
   float fsrC = 0;
   float fsrE = 0;
   float fsrG = 0;
-
-  Serial.println(analogRead(A0));
-
-
+  
   fsrA = analogRead(A0) * VCC / 1023.0;
   fsrC = analogRead(A1) * VCC / 1023.0;
   fsrE = analogRead(A2) * VCC / 1023.0;
   fsrG = analogRead(A3) * VCC / 1023.0;
-
-  Serial.println(fsrA);
-  Serial.println(fsrC);
-  Serial.println(fsrE);
-  Serial.println(fsrG);
 
   float fsrRA = R_DIV * (VCC / fsrA - 1.0);
   float fsrRC = R_DIV * (VCC / fsrC - 1.0);
@@ -112,11 +95,6 @@ void loop() {
   float fsrCE = 1.0 / fsrRE;
   float fsrCG = 1.0 / fsrRG;
 
-  Serial.println(force(fsrRA, fsrCA));
-  Serial.println(force(fsrRC, fsrCC));
-  Serial.println(force(fsrRE, fsrCE));
-  Serial.println(force(fsrRG, fsrCG));
-
   float fsrFA = force(fsrRA, fsrCA)*taraA/1000*9.81;
   float fsrFC = force(fsrRC, fsrCC)*taraC/1000*9.81;
   float fsrFE = force(fsrRE, fsrCE)*taraE/1000*9.81;
@@ -125,23 +103,9 @@ void loop() {
   float x = (fsrFC*0.60 + fsrFE*0.60 - (7.1*9.81*0.60/2))/(63*9.81);
   float y = (fsrFA*0.60 + fsrFC*0.60 - (7.1*9.81*0.60/2))/(63*9.81);
 
-  //float x = (fsrFC*0.60 + fsrFD*0.60 + fsrFE*0.60 + fsrFB*0.60/2 + fsrFF*0.60/2 )/(63*9.81);
-  //float y = (fsrFA*0.60 + fsrFB*0.60 + fsrFC*0.60 + fsrFD*0.60/2 + fsrFH*0.60/2 )/(63*9.81);
-
   // print out the value you read:
-  String text = "A: ";
-  text = text + fsrFA;
-  text = text + " C: "; 
-  text = text + fsrFC;  
-  text = text + " E: ";
-  text = text + fsrFE;
-  text = text + " G: "; 
-  text = text + fsrFG;
-  Serial.println(text);
   Serial.print("X = ");
-  Serial.println(x);
-  Serial.print("Y = ");
+  Serial.print(x);
+  Serial.print(" Y = ");
   Serial.println(y);
-
-  delay(1000);  // delay in between reads for stability
 }
